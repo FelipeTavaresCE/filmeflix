3bc4ace7cf6e7ef7ffaae70ef342c2358eddee85
Felipe Tavares Mesquita <felipemesquita@ext.cvccorp.com.br>
2021-07-29 15:30:04 -0300

    Estrutura inicial

---
+++ src/main/java/com/example/filmeflix/FilmeflixApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/filmeflix/FilmeflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmeflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
