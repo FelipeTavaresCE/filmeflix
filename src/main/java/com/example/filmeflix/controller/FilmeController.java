8522b05c2db5de1cb32f4331ff68e642ba0dc6c9
Felipe Tavares Mesquita <tavares.ads@gmail.com>
2021-08-19 11:29:05 -0300

    add metodo find filmes

--- src/main/java/com/example/filmeflix/controller/FilmeController.java
+++ src/main/java/com/example/filmeflix/controller/FilmeController.java
@@ -2,16 +2,12 @@ package com.example.filmeflix.controller;
 
 import javax.validation.Valid;
 
+import com.example.filmeflix.model.Filme;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.filmeflix.dto.FilmeDTO;
 import com.example.filmeflix.dto.FilmeResponse;
@@ -22,11 +18,12 @@ import br.com.cvc.corp.salespartner.branch.dto.ApiError;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Validated
 @Tag(name = "Movies API", description = "")
@@ -60,5 +57,18 @@ public class FilmeController {
 		service.delete(id);
 		return new ResponseEntity<String>(HttpStatus.OK);
 	}
-	
+
+	@Operation(summary = "Find new list movie")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Ok.", content = @Content),
+		@ApiResponse(responseCode = "401", description = "Unauthorized.", content = @Content),
+		@ApiResponse(responseCode = "404", description = "Movie Not Found.", content = @Content),
+		@ApiResponse(responseCode = "500", description = "Internal Error", content = @Content),
+		})
+	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Filme>> deleteMovie(){
+		List<Filme> movies = service.findLatestMovies();
+		return ResponseEntity.ok(movies);
+	}
+
 }
