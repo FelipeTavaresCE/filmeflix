0a1b80f4c428a6dcffe8f03a11e8e22baebeec09
Felipe Tavares Mesquita <tavares.ads@gmail.com>
2021-08-19 16:32:05 -0300

    mongo

--- src/main/java/com/example/filmeflix/repository/FilmeRepository.java
+++ src/main/java/com/example/filmeflix/repository/FilmeRepository.java
@@ -1,11 +1,10 @@
 package com.example.filmeflix.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.filmeflix.model.Filme;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.filmeflix.model.Filme;
-
 @Repository
-public interface FilmeRepository extends JpaRepository<Filme, Long>{
+public interface FilmeRepository extends MongoRepository<Filme, Long>{
 	
 }
